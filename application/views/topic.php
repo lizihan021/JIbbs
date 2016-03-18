20fdef1e299675d0a1a7ba3d7204e43553643bb4
Liu Yihao <liuyh615@126.com>
2016-03-18 14:49:46 +0800

    topic

---
+++ application/views/topic.php
@@ -0,0 +1,36 @@
+<?php include 'common/header_common.php';?>
+<?php include 'common/generate.php';?>
+
+	<script type='text/javascript'>
+		
+		$(document).ready(function()
+		{
+			
+			
+		});
+	</script>
+	<div class="container">
+    	<div class="panel panel-default">
+        	<div id="main_heading" class="panel-heading">
+            	<?php echo $site_title;?>
+            </div>
+            <div class="panel-body">
+            	<div class="row">
+                	<div class="col-md-3">
+                    	<center>
+                        	<img class="img-rounded" src="http://127.0.0.1/avatar/liuyh615-d742375d14529594d73bc7a0c72c54fe_big.jpg" alt="liuyh615_avatar">
+                        </center>
+                	</div>
+                    <div class="col-md-9">
+                    	dsgfhkgfhjkgfdskjhgfdskj<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+                    </div>
+                </div>
+            </div>
+    	</div>
+	</div>
+                
+    </div><!-- /.container -->
+
+<?php include 'common/footer_common.php';?>
+</body>
+</html>
