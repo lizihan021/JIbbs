10255844044dbaf62f868de689d4b930ed5930d2
tc-imba <liuyh615@126.com>
2016-03-22 18:43:36 +0800

    reply

--- application/controllers/Ajax.php
+++ application/controllers/Ajax.php
@@ -172,4 +172,33 @@ class Ajax extends CI_Controller
 		}
 		echo implode('|', $reply_str);		
 	}
+	
+	public function reply_submit($data)
+	{
+		$this->load->model('topic_model');
+		$this->load->model('reply_model');
+		$topic = $this->topic_model->get_topic_by_id($data['topic_id']);
+		if ($topic == 0 )
+		{
+			echo 'topic undefined';
+			return;
+		}
+		$data_ = array
+		(
+			'topic_id'    => $topic->topic_id,
+			'user_id'     => $this->session->userdata('uid'),
+			'content'     => base64_encode($data['content'])
+
+		);
+		if ($data['reply_id'] > 0)
+		{
+			$data['floor_id'] = $data['reply_id'];
+			$this->reply_model->update($data_);
+		}
+		else
+		{
+			$data['floor_id'] = $topic->reply_num + 1;
+			$this->reply_model->create($data_);
+		}
+	}
 }
