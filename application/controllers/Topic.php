bf07cd7f687cc1e212e87c280c3a8046dffe4b9f
Liu Yihao <liuyh615@126.com>
2016-03-25 21:31:20 +0800

    uc0asdhuashud
    sakdnjkasjd

--- application/controllers/Topic.php
+++ application/controllers/Topic.php
@@ -20,10 +20,10 @@ class Topic extends Front_Controller
 		}
 		else
 		{
-			$user = $this->user_model->get_user_by_id($topic->user_id);
+			//$user = $this->user_model->get_user_by_id($topic->user_id);
 			$data['site_title'] = $topic->name;
-			$data['user_name'] = $user->username;
-			$data['user_avatar'] = $user->avatar; 
+			$data['user_name'] = $this->session->userdata('username');
+			//$data['user_avatar'] = $user->avatar; 
 			$data['reply_num'] = $topic->reply_num;
 			if ($num == NULL)
 			{
