c3bd10edc80fd3b99d686b4597c83d0a5669a26b
Liu Yihao <liuyh615@126.com>
2016-04-09 14:49:31 +0800

    Update Module_model.php

--- application/models/Module_model.php
+++ application/models/Module_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Module_Model extends CI_Model
+class Module_model extends CI_Model
 {
 		
     function __construct()
