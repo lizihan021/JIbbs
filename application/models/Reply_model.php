3dc979f2ff37885bfef2aa6f2137695e4090e253
Liu Yihao <liuyh615@126.com>
2016-04-09 14:49:05 +0800

    Update Reply_model.php

--- application/models/Reply_model.php
+++ application/models/Reply_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Reply_Model extends CI_Model
+class Reply_model extends CI_Model
 {
 		
     function __construct()
