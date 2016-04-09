49edad1464fea37cdd38f879ccd0809dfe49da5f
Liu Yihao <liuyh615@126.com>
2016-04-09 14:50:33 +0800

    Update and rename topic_model.php to Topic_model.php

--- application/models/Topic_model.php
+++ application/models/Topic_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Topic_Model extends CI_Model
+class Topic_model extends CI_Model
 {
 		
     function __construct()
